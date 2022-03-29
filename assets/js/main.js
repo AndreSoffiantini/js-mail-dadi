dc6d8d21294d5308186227238f7e6c21c32cbcb6
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-03-29 15:56:16 +0200

    Add ex. 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+/********* ESERCIZIO 1 **********/
+
 // Chiedere all'utente la mail
 
 const userMail = prompt("Inserisci la tua mail");
@@ -16,8 +18,7 @@ for (let i = 0; i < validMails.length; i++) {
 
     let validMail = validMails[i];
 
-    /* La variabile di controllo assume valore true se la userMail è uguale ad almeno una validMail,
-    altrimenti rimane false */
+    // La variabile di controllo assume valore true se la userMail è uguale ad almeno una validMail, altrimenti rimane false
     if (userMail === validMail) {
 
         validUser = true;
@@ -30,8 +31,51 @@ console.log(validUser);
 
 // Stampare messaggio di esito del controllo
 
-// Generare un numero random tra 1 e 6 per il giocatore
+if (validUser) {
 
-// Generare un numero random tra 1 e 6 per il computer
+    alert(`Utente ${userMail} ammesso`);
+
+} else if (!validUser) {
+
+    alert(`Utente ${userMail} non ammesso`);
+
+}
+
+
+/********* ESERCIZIO 2 **********/
+
+// Generare un numero random tra 1 e 6 per il giocatore, poi stamparlo a schermo
+
+const userNumber = Math.round(1 + 5 * Math.random());
+console.log(userNumber);
+document.getElementById("userScore").innerHTML = `Il tuo punteggio è ${userNumber}`;
+
+// Generare un numero random tra 1 e 6 per il computer, poi stamparlo a schermo
+
+const computerNumber = Math.round(1 + 5 * Math.random());
+console.log(computerNumber);
+document.getElementById("computerScore").innerHTML = `Il punteggio del computer è ${computerNumber}`;
 
 // Stabilire il punteggio più alto, che sarà il vincitore
+
+let winner = document.getElementById("winner");
+
+if (userNumber > computerNumber) {
+
+    // Stampa a schermo il messaggio di vittoria se il numero dell'utente è maggiore di quello del computer
+    winner.innerHTML = `Congratulazioni, hai vinto!!`;
+    winner.classList.add("success");
+
+} else if (computerNumber > userNumber) {
+
+    // Stampa a schermo il messaggio di sconfitta se il numero dell'utente è minore di quello del computer
+    winner.innerHTML = `Hai perso, riprova!!`;
+    winner.classList.add("defeat");
+
+} else {
+
+    // Stampa a schermo il messaggio di pareggio se il numero dell'utente è uguale a quello del computer
+    winner.innerHTML = `Hai pareggiato, riprova!!`;
+    winner.classList.add("tie");
+
+}
