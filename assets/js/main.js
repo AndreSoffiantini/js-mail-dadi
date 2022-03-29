258bf5976560f492b342d8e5accbdb6028f04e06
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-03-29 15:13:36 +0200

    Add control loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,32 @@
 // Chiedere all'utente la mail
 
-// Controllare che sia nella lista di chi può accedere
+const userMail = prompt("Inserisci la tua mail");
+console.log(userMail);
+
+// Definire una lista di utenti ammessi
+
+const validMails = ["mail.valida1@email.com", "mail.valida2@email.com", "mail.valida3@email.com"];
+
+// Controllare che sia nella lista di utenti ammessi
+
+// Impostare una variabile di controllo booleana che verifichi l'ammissibilità dell'utente
+let validUser = false;
+
+for (let i = 0; i < validMails.length; i++) {
+
+    let validMail = validMails[i];
+
+    /* La variabile di controllo assume valore true se la userMail è uguale ad almeno una validMail,
+    altrimenti rimane false */
+    if (userMail === validMail) {
+
+        validUser = true;
+
+    }
+
+}
+
+console.log(validUser);
 
 // Stampare messaggio di esito del controllo
 
