780137dae98b99ce5075637c8841c271b15d7be2
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-03-29 14:48:54 +0200

    Initial commit
    Defined base instructions in main.js

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// Chiedere all'utente la mail
+
+// Controllare che sia nella lista di chi può accedere
+
+// Stampare messaggio di esito del controllo
+
+// Generare un numero random tra 1 e 6 per il giocatore
+
+// Generare un numero random tra 1 e 6 per il computer
+
+// Stabilire il punteggio più alto, che sarà il vincitore
